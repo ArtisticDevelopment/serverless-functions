551a6368908bde95b937ac35fc2ab9fbfd265a45
Silas Green <scgreen160@gmail.com>
2022-12-09 15:06:47 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Reactç from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Up and Running with Serverless Functions</h1>
+      <input type="button" id="fetch-btn">
+        Fetch
+      </input>
+      <p id="response-output">Response Placeholder</p>
+    </div>
+  );
+}
+
+export default App;
